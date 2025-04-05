5af374d9a9c4897f9b23f5314a778db14b750c11
CoderNandan <nandanindia1947@gmail.com>
2025-04-05 22:36:57 +0530

    add a components layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import "./globals.css";
 import { WaitList } from "./_root/pages/index";
 import RootLayout from "./_root/RootLayout";
+import ComponentLayout from "./_root/ComponentLayout";
 function App() {
   return (
     <main className="">
       <Routes>
         <Route element={<RootLayout />}>
           <Route path="/" element={<WaitList />} />
+          <Route path="/components/" element={<ComponentLayout />}></Route>
         </Route>
       </Routes>
-      
     </main>
   );
 }
