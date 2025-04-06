3d01dc477ef51dd8d0bf3cefde4d4fca11a89856
soumya0897 <soumyadeepdey198@gmail.com>
2025-04-06 23:17:42 +0530

    Accordian and Footer Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import "./globals.css";
 import { WaitList } from "./_root/pages/index";
 import RootLayout from "./_root/RootLayout";
-// import FAQ from "./components/FAQ";
-import Accordian from "./components/Accordion/Accordian";
-// import FAQ from "./components/FAQ";
-import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <main className="">
