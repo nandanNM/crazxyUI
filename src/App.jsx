37b524858ee38eabbe0613de1377775efb7e229a
Swarnavo2003 <swarnavo2020@gmail.com>
2025-04-08 00:42:09 +0530

    Changes done in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import "./globals.css";
 import { WaitList } from "./_root/pages/index";
 import RootLayout from "./_root/RootLayout";
 import ComponentLayout from "./_root/ComponentLayout";
-import Loader from "./components/ui/Loader/Loader";
-import Carousel from "./components/ui/Carousel/Carousel";
 
 function App() {
   return (
@@ -13,8 +11,6 @@ function App() {
         <Route element={<RootLayout />}>
           <Route path="/" element={<WaitList />} />
           <Route path="/components/" element={<ComponentLayout />}></Route>
-          <Route path="/loader" element={<Loader />} />
-          <Route path="/carousel" element={<Carousel />} />
         </Route>
       </Routes>
     </main>
